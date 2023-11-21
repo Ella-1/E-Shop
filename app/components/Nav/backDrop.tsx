44d97372dad0686ab3aab0ca0d06ad3e1e94ef84
Ella-1 <batazoaka3@gmail.com>
2023-11-21 14:21:34 +0100

    navigation tab

---
+++ app/components/Nav/backDrop.tsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+
+ interface BackDropProps{
+       onClick: () => void
+ }
+ 
+ export const BackDrop: React.FC<BackDropProps > = () => {
+   return (
+     <div>BackDrop</div>
+   )
+ }
+ 
--- app/register/registerForm.tsx
+++ app/register/registerForm.tsx
@@ -6,6 +6,10 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,10 +19,33 @@ const RegisterForm = () => {
     formState: { errors },
   } = useForm<FieldValues>({});
 
+  const router = useRouter()
+
   const onSubmit:SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true)
-    console.log(data) 
-  }
+    // console.log(data) 
+axios.post('/api/register', data)
+  .then(() => {
+    toast.success('Account Created Successfully');
+    return signIn('credentials', {
+      email: data.email,
+      password: data.password,
+      redirect: false
+    });
+  })
+  .then((callback) => {
+    if (callback?.ok) {
+      router.push('/carts');
+      router.refresh();
+      toast.success('Logged In successfully');
+    }
+    if (callback?.error) {
+      toast.error(callback.error);
+    }
+  }).catch(() => toast.error("Something went wrong") ).finally(() =>  setIsLoading(false))
+ 
+  
+}
 
   return (
     <>
@@ -61,6 +88,6 @@ const RegisterForm = () => {
       <p className="text-sm mt-2">Already have an account? <Link className="underline" href={'/login'}> Login</Link></p>
     </>
   );
-};
+  }
 
 export default RegisterForm;
