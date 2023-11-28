471619f4d1f1b7decce626d7705aaee251d97298
Ella-1 <batazoaka3@gmail.com>
2023-11-28 15:51:53 +0100

    add a product

--- app/admin/add-products/addProductForm.tsx
+++ app/admin/add-products/addProductForm.tsx
@@ -1,9 +1,78 @@
-"use client"
+"use client";
 
-import React from 'react'
+import CustomCheckBox from "@/app/components/inputs/customCheckBox";
+import Input from "@/app/components/inputs/input";
+import TextArea from "@/app/components/inputs/textArea";
+import Heading from "@/app/components/products/heading";
+import React, { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 export default function AddProductForm() {
+  const [isLoading, setIsloading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      brand: "",
+      category: "",
+      inStock: false,
+      images: [],
+      price: "",
+    },
+  });
+
   return (
-    <div>AddProduct</div>
-  )
+    <>
+      <Heading title="Add a Product" />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="number"
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    
+    <TextArea 
+    id="description"
+    label="Description"
+    disabled={isLoading}
+    register={register}
+    errors={errors}
+    required
+    />
+    <CustomCheckBox id="inStock" register={register} label="This product is in Stock" />
+    
+    <div className="w-full font-medium">
+      <div className="mb-2 font-semibold">Select a Category</div>
+    <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] overflow-y-auto">
+      {}
+    </div>
+    </div>
+    </>
+  );
 }
---
+++ app/components/inputs/customCheckBox.tsx
@@ -0,0 +1,30 @@
+"use client"
+import React from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface CustomCheckBoxProps{
+    id: string;
+    label: string;
+    disabled?: boolean
+    register: UseFormRegister<FieldValues>
+}
+
+const CustomCheckBox:React.FC<CustomCheckBoxProps> = ({id, label, register, disabled
+}) => {
+  return (
+    <div className='w-full flex  flex-row gap-2 items-center'>
+        <input type="checkbox" 
+         id={id}
+         {...register(id)}
+         placeholder=""
+         disabled={disabled}
+         className='cursor-pointer'
+        />
+        <label htmlFor={id} className='font-medium cursor-pointer'>
+             {label}
+        </label>
+    </div>
+  )
+}
+
+export default CustomCheckBox
