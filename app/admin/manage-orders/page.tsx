e4f451359d638567ed04e923ab41533adf4e6d6c
Ella-1 <batazoaka3@gmail.com>
2023-12-05 13:27:07 +0100

    add product

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { ManageProductClient } from "./manageProductClient";
+import GetProduct from "@/actions/getProduct";
+import { GetCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/nullData";
+import Container from "@/app/components/Container";
 
-export default function ManageOrders() {
+export default async function page() {
+  const products = await GetProduct({ category: null });
+  const currentUser = await GetCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access denied" />;
+  }
+
   return (
-    <div>ManageO</div>
-  )
+    <div className="pt-8">
+      <Container>
+        <ManageProductClient products={products} />
+      </Container>
+    </div>
+  );
 }
