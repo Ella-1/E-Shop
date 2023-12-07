f48abe5ee2ddf16fd814be0650864e895f418f07
Ella-1 <batazoaka3@gmail.com>
2023-12-07 16:16:39 +0100

    search product algorithm

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
 import Image from "next/image";
 import Container from "./components/Container";
-import HomeBanner from "./components/Homebanner";
-import { products } from "@/utils/products";
+import HomeBanner from "./components/HomeBanner";
 import { Truncate } from "@/utils/truncateTex";
 import ProductCards from "./components/products/ProductCards";
+import GetProduct, { IProductParams } from "@/actions/getProduct";
+import NullData from "./components/nullData";
 
-export default function Home() {
+interface HomeProps{
+  searchParams: IProductParams
+}
+
+export default async function Home({searchParams}: HomeProps) {
+  const products = await GetProduct(searchParams)
+
+  // Algorithm thats displays products at random
+  if(products?.length === 0) {
+    return <NullData title='Oops! No Products Found. Click "All" to clear filter' />
+  }
+
+  // fisher-yates shuffle algorithm
+  function shuffleArray(array:any) {
+    for(let i = array.length -1; i> 0; i--){
+      const j = Math.floor(Math.random() * (i+1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+    return array;
+  }
+
+  const shuffledProducts = shuffleArray(products)
+
+
+
+
   return (
     <div className="p-8">
       <Container>
@@ -14,7 +40,7 @@ export default function Home() {
         </div>
         <div>
        <div  className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-       {products.map((product: any )=> {
+       {shuffledProducts.map((product: any )=> {
           return (
             <div>
               <ProductCards data={product}/>
