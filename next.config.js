5297bcd2add030fe74788f09ca3f415be1346b63
Ella-1 <102160639+Ella-1@users.noreply.github.com>
2023-12-14 22:12:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
     images: {
         domains: ["firebasestorage.googleapis.com","lh3.googleusercontent.com"]
-    }
+    },
+    reactStrictMode: true, 
+    eslint: { 
+      ignoreDuringBuilds: true, 
+    }, 
 }
+
 
 module.exports = nextConfig
