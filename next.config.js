238e2354406684dd87bb33e920718cff5496851d
Ella-1 <102160639+Ella-1@users.noreply.github.com>
2023-12-14 21:17:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 const nextConfig = {
     images: {
         domains: ["firebasestorage.googleapis.com","lh3.googleusercontent.com"]
-    },
-    eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+    }
 }
 
 module.exports = nextConfig
